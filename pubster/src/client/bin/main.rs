8bffe1e7e838954dc2fbc23cea06a5e7d4570eda
Divy Gobiraj <divygobi@gmail.com>
2026-02-28 22:26:54 -0800

    Add client code and basic bash test

--- pubster/src/client/bin/main.rs
+++ pubster/src/client/bin/main.rs
@@ -1,177 +1,121 @@
 use std::io;
-use std::fs::{self, OpenOptions};
-use std::io::prelude::*;
 
 use client::pub_sub_client::PubSubClient;
-
-use client::{ClientEvent, ConnectCmd, SubscribeCmd, UnsubscribeCmd, PublishCmd,
-    ServerEvent, ListTopicsRequest, ListTopicsResponse};
+use client::{
+    ClientEvent, client_event::Payload,
+    ConnectCmd, SubscribeCmd, UnsubscribeCmd, PublishCmd,
+    ListTopicsRequest,
+};
+use tokio::sync::mpsc;
 use tokio_stream::StreamExt;
+use tokio_stream::wrappers::ReceiverStream;
 
 pub mod client {
     tonic::include_proto!("pubster");
 }
 
- #[tokio::main]
+#[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //TODO add api to set server ip
-    println!("Hello user, I'm the client! I'm gonna try connecting to the server now.");
+    println!("Enter your client name:");
+    let mut client_name = String::new();
+    io::stdin().read_line(&mut client_name).expect("Failed to read name");
+    let client_name = client_name.trim().to_string();
+
+    println!("Connecting to server...");
     let mut server = PubSubClient::connect("http://[::1]:50051").await?;
-    
-    let client_id = server.get_client_id(tonic::Request::new(ClientIdRequest{
-         hello: String::from("Meow"),
-    })).await?.into_inner().client_id;
 
-    println!("{}", format!("Connected successfully!! You client id is {}.",client_id));
+    // Channel: tx → we send ClientEvents; rx → tonic sends them to the server
+    let (tx, rx) = mpsc::channel::<ClientEvent>(32);
 
-    let mut option = String::new();
-       
+    // ConnectCmd must be the first message on the stream
+    tx.send(ClientEvent {
+        payload: Some(Payload::Connect(ConnectCmd {
+            client_name: client_name.clone(),
+        })),
+    }).await?;
+
+    // Open the bidi stream; pass rx as the outgoing stream
+    let mut in_stream = server.handshake(ReceiverStream::new(rx)).await?.into_inner();
+
+    // Background task: print every ServerEvent that arrives
+    tokio::spawn(async move {
+        while let Some(result) = in_stream.next().await {
+            match result {
+                Ok(event) => {
+                    println!("[{}] {}: {}", event.topic_name, event.publisher_name, event.payload);
+                }
+                Err(e) => {
+                    eprintln!("Stream error: {}", e);
+                    break;
+                }
+            }
+        }
+    });
+
+    // Menu loop
     loop {
-        print_options(client_id); // Print options again after processing the input
-        option.clear();
-        io::stdin().read_line(&mut option).expect("Failed to read option");
+        print_options(&client_name);
+        let mut option = String::new();
+        let n = io::stdin().read_line(&mut option).expect("Failed to read option");
+        if n == 0 { break; } // EOF (e.g. piped input exhausted)
 
         match option.trim() {
             "0" => {
-                // List topics
-                println!("Listing topics...");
-                // Here you would call the server to list topics
-            },
+                let response = server.list_topics(ListTopicsRequest {}).await?.into_inner();
+                if response.topic_names.is_empty() {
+                    println!("No topics.");
+                } else {
+                    println!("Topics: {}", response.topic_names.join(", "));
+                }
+            }
             "1" => {
-                // Subscribe to topic
                 println!("Enter topic name to subscribe:");
-                let mut topic_input = String::new();
-                io::stdin().read_line(&mut topic_input).expect("Failed to read topic");
-                let topic_name = topic_input.trim().to_string();
-
-                println!("Subscribing to topic '{}'...", topic_name);
-                let sub_res = server.subscribe(SubscribeTopicRequest{
-                    topic: Some(Topic {
-                        topic_name: topic_name.clone(),
-                    }),
-                    client_id: client_id
+                let mut topic = String::new();
+                io::stdin().read_line(&mut topic).expect("Failed to read topic");
+                tx.send(ClientEvent {
+                    payload: Some(Payload::Subscribe(SubscribeCmd { topic_name: topic.trim().to_string() })),
                 }).await?;
-
-                // Get the stream from the response
-                let mut stream = sub_res.into_inner();
-
-                // Create directory for this client if it doesn't exist
-                let dir_path = format!("testResults/{}", client_id);
-                fs::create_dir_all(&dir_path).expect("Failed to create directory");
-
-                let topic_for_task = topic_name.clone();
-
-                // Spawn a background task to process the stream
-                tokio::spawn(async move {
-                    println!("Listening for messages on topic '{}'...", topic_for_task);
-                    while let Some(result) = stream.next().await {
-                        match result {
-                            Ok(msg) => {
-                                println!("[{}] Received: {}", topic_for_task, msg.message);
-
-                                // Write to file (append mode)
-                                if let Err(e) = append_to_file(&topic_for_task, &msg.message, client_id) {
-                                    eprintln!("Failed to write to file: {}", e);
-                                }
-                            }
-                            Err(e) => {
-                                eprintln!("Stream error: {}", e);
-                                break;
-                            }
-                        }
-                    }
-                    println!("Stream for topic '{}' ended.", topic_for_task);
-                });
-
-                println!("Subscribed to '{}'. Messages will be written to {}/{}.txt",
-                         topic_name, dir_path, topic_name);
-            },
+            }
             "2" => {
-                // Publish a message
-                println!("Publishing a message...");
-                // Here you would call the server to publish a message
-            },
+                println!("Enter topic name:");
+                let mut topic = String::new();
+                io::stdin().read_line(&mut topic).expect("Failed to read topic");
+                println!("Enter message:");
+                let mut msg = String::new();
+                io::stdin().read_line(&mut msg).expect("Failed to read message");
+                tx.send(ClientEvent {
+                    payload: Some(Payload::Publish(PublishCmd {
+                        topic_name: topic.trim().to_string(),
+                        payload: msg.trim().to_string(),
+                    })),
+                }).await?;
+            }
             "3" => {
-                // Unsubscribe from topic
-                println!("Unsubscribing from topic...");
-                // Here you would call the server to unsubscribe from a topic
-            },
+                println!("Enter topic name to unsubscribe:");
+                let mut topic = String::new();
+                io::stdin().read_line(&mut topic).expect("Failed to read topic");
+                tx.send(ClientEvent {
+                    payload: Some(Payload::Unsubscribe(UnsubscribeCmd { topic_name: topic.trim().to_string() })),
+                }).await?;
+            }
             "4" => {
-                // Exit
                 println!("Exiting...");
-                break ;
-            },
+                break;
+            }
             _ => {
-                println!("Invalid option, please try again.");
-//                print_options(client_id);
+                println!("Invalid option.");
             }
         }
     }
-    
+
     Ok(())
 }
 
-
-fn print_options(client_id: u32){
-        println!("Hello client, here are your options, I am client: {}", client_id);
-        println!("0: List Topics");
-        println!("1: Subscribe to topic");
-        println!("2: Publish a message");
-        println!("3: Unsubscribe from topic");
-        println!("4: Exit");
-}
-
-
-fn append_to_file(topic_name: &str, message: &str, client_id: u32) -> io::Result<()> {
-    let file_path = format!("testResults/{}/{}.txt", client_id, topic_name);
-    let mut file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(file_path)?;
-    writeln!(file, "{}", message)?;
-    Ok(())
-}
-
-fn test(){
-
-    // let mut client = PubSubClient::connect("http://[::1]:50051").await?;
-    // let pau_theory_topic: Option<Topic> =  Some(Topic{
-    //         topic_name: "Pau Theory".into(),
-    //     });
-
-
-    // let client_id = client.get_client_id(tonic::Request::new(ClientIdRequest{
-    //     hello: String::from("Meow"),
-    // })).await?.into_inner().client_id;
-
-
-    // let sub_request = tonic::Request::new(SubscribeTopicRequest{
-    //     topic: pau_theory_topic.clone(),
-    //     client_id: client_id,
-    // });
-
-    // let sub_response = client.subscribe(sub_request).await?;
-    
-    // // if sub_response.into_inner().ack != None{
-    // //     tokio::spawn(future)
-    // // }
-
-    // let pub_request = tonic::Request::new(PublishMessageRequest{
-    //     message: Some(Message{
-    //         message: String::from("Pau evolves into Gao"),
-    //         topic: pau_theory_topic.clone(),
-    //     }),
-    //     topic: pau_theory_topic.clone(),
-    //     client_id: client_id,
-    // });
-
-    // let pub_response = client.publish(pub_request).await?;
-
-
-    // let unsub_request = tonic::Request::new(UnsubscribeTopicRequest{
-    //     topic: pau_theory_topic.clone(),
-    //     client_id: client_id,
-    // });
-
-    // let unsub_response = client.unsubscribe(unsub_request).await?;
+fn print_options(client_name: &str) {
+    println!("\nHello {}, here are your options:", client_name);
+    println!("0: List Topics");
+    println!("1: Subscribe to topic");
+    println!("2: Publish a message");
+    println!("3: Unsubscribe from topic");
+    println!("4: Exit");
 }
